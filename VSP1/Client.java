f1a0f6e2e50b4764e95396d6ea63a067525b25e9
Lennart Hartmann <lennart.hartmann@haw-hamburg.de>
2017-10-06 14:50:28 +0200

    Added JavaFxGui

--- VSP1/Client.java
+++ VSP1/Client.java
@@ -1,61 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-
-
+import java.rmi.RemoteException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Client {
+/**
+ *
+ * @author lennart
+ */
+public class Client extends Application {
 
-    public static void main(String args[]) {
+    static MessageService server;
+    static Registry registry;
 
+    Stub stub = new Stub();
+    Button receiveBtn = new Button("receive next message");
+    Button sendBtn = new Button("send Message");
+    Button connectBtn = new Button("connect");
+    Button showAllBtn = new Button("show all");
+
+    /**
+     * For testing purposes only
+     */
+    public static void initTestadata() {
+        String result;
         try {
-            Registry registry = LocateRegistry.getRegistry();
-            MessageService server = (MessageService) Naming.lookup("MessageService");
-            String result;
             server.newMessage("client1", "Nachricht1");
-            result = server.nextMessage("client2");
-            System.out.println("Client2 : " + result);
+            server.newMessage("client2", "Nachricht2");
+            server.newMessage("client3", "Nachricht3");
             server.newMessage("client1", "Nachricht4");
-            server.newMessage("client1", "Nachricht5");
-            server.newMessage("client1", "Nachricht6");
-            result = server.nextMessage("client2");
-            System.out.println("Client2 : " + result);
-            server.newMessage("client1", "Nachricht7");
-            server.newMessage("client1", "Nachricht8");
-            server.newMessage("client1", "Nachricht9");
-            server.newMessage("client1", "Nachricht10");
-            result = server.nextMessage("client2");
-            System.out.println("Client2 : " + result);
-            server.newMessage("client1", "Nachricht11");
-            server.newMessage("client1", "Nachricht13");
-            server.newMessage("client1", "Nachricht14");
-            server.newMessage("client1", "Nachricht15");
-            server.newMessage("client1", "Nachricht16");
-            server.newMessage("client1", "Nachricht17");
-            server.newMessage("client1", "Nachricht18");
-            server.newMessage("client1", "Nachricht19");
-            server.newMessage("client2", "Nachricht20");
-            result = server.nextMessage("client2");
-            System.out.println("Client2 : " + result);
-            server.newMessage("client3", "Nachricht19");
-            server.newMessage("client4", "Nachricht20");
             result = server.nextMessage("client1");
-            System.out.println("Client2 : " + result);
-            //result = server.nextMessage("client2");
-            //System.out.println("Client2 : " + result);
-
-//            result = server.nextMessage("client1");
-//            System.out.println("Client1 : " + result);
-//            result = server.nextMessage("client1");
-//            System.out.println("Client1 : " + result);
-//            result = server.nextMessage("client1");
-//            System.out.println("Client1 : " + result);
-//            result = server.nextMessage("client1");
-//            System.out.println("Client1 : " + result);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println(result);
+            result = server.nextMessage("client1");
+            System.out.println(result);
+            result = server.nextMessage("client1");
+            System.out.println(result);
+            result = server.nextMessage("client1");
+            System.out.println(result);
+            result = server.nextMessage("client1");
+            System.out.println(result);
+            result = server.nextMessage("client2");
+            System.out.println(result);
+            result = server.nextMessage("client2");
+            System.out.println(result);
+            System.out.println("3 = 2 + 1 = " + server.add(2, 1));
+        } catch (RemoteException e) {
+            System.out.println("Client.main()");
         }
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+//        initTestadata();
+
+        //Creating a GridPane container
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+        //Defining the Name text field
+        final TextField nextMsgField = new TextField();
+        
+        nextMsgField.setMinWidth(400);
+        
+        nextMsgField.setPromptText("");
+        nextMsgField.setPrefColumnCount(10);
+        nextMsgField.getText();
+        GridPane.setConstraints(nextMsgField, 0, 0);
+        grid.getChildren().add(nextMsgField);
+        //Defining the Last Name text field
+        final TextField newMsgField = new TextField();
+        newMsgField.setPromptText("Enter your message here");
+
+        GridPane.setConstraints(newMsgField, 0, 1);
+        grid.getChildren().add(newMsgField);
+        //Defining the Comment text field
+        final TextField serverField = new TextField();
+        serverField.setPrefColumnCount(15);
+        serverField.setPromptText("Enter your serverField.");
+        GridPane.setConstraints(serverField, 0, 2);
+        grid.getChildren().add(serverField);
+        GridPane.setConstraints(receiveBtn, 1, 0);
+        grid.getChildren().add(receiveBtn);
+        GridPane.setConstraints(sendBtn, 1, 1);
+        grid.getChildren().add(sendBtn);
+        GridPane.setConstraints(connectBtn, 1, 2);
+        grid.getChildren().add(connectBtn);
+        GridPane.setConstraints(showAllBtn, 1, 3);
+        grid.getChildren().add(showAllBtn);
+
+        receiveBtn.setOnAction(e -> {
+            System.out.println("receiving message");
+            try {
+                nextMsgField.setText(server.nextMessage(this.toString()));
+            } catch (RemoteException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        sendBtn.setOnAction(e -> {
+            System.out.println(newMsgField.getText());
+            try {
+                server.newMessage(this.toString(), newMsgField.getText());
+            } catch (RemoteException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        connectBtn.setOnAction(e -> {
+                System.out.println("connecting to server");
+        });
+
+        showAllBtn.setOnAction(e -> {
+            stub.printAll();
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(grid);
+
+        Scene scene = new Scene(root, 550, 150);
+
+        primaryStage.setTitle("Message Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        try {
+            registry = LocateRegistry.getRegistry();
+            server = (MessageService) Naming.lookup("MessageService");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Application.launch(args);
+    }
 }
