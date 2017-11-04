8d417e210f51ad5bbaa274cbbc60fe72c968a207
Nils Eggebrecht <nils.eggebrecht@haw-hamburg.de>
2017-11-04 12:27:39 +0100

    pollard roh reduce redunancen

--- VSP2/pollard_roh.cpp
+++ VSP2/pollard_roh.cpp
@@ -51,6 +51,8 @@ Z4 = 100060210614380659647872297427366695090390611213179474545733865926684244698
 */
 
 
+
+
 /* method to return prime divisor for N */
 numberToSplit PollardRho(int512_t N)
 {
@@ -95,7 +97,6 @@ numberToSplit PollardRho(int512_t N)
     int512_t M = (int512_t) sqrt(sqrt(N));
     M = (int512_t) (M * 118);
     M = (int512_t) (M / 100);
-
 
     do
     {
@@ -224,13 +225,6 @@ inline bool IsPrime( int512_t number )
     if(witness(61,number)) return false;
     return true;
 
-    /*WARNING: Algorithm deterministic only for numbers < 4,759,123,141 (unsigned int's max is 4294967296)
-      if n < 1,373,653, it is enough to test a = 2 and 3.
-      if n < 9,080,191, it is enough to test a = 31 and 73.
-      if n < 4,759,123,141, it is enough to test a = 2, 7, and 61.
-      if n < 2,152,302,898,747, it is enough to test a = 2, 3, 5, 7, and 11.
-      if n < 3,474,749,660,383, it is enough to test a = 2, 3, 5, 7, 11, and 13.
-      if n < 341,550,071,728,321, it is enough to test a = 2, 3, 5, 7, 11, 13, and 17.*/
 }
 
 /////////////////////////
@@ -331,43 +325,23 @@ int main()
         //TODO: Manager.size() nutzen nicht int anzahlManager nutzen
         int anzahlManager = 1;
 
-        // weniger Manager als Faktoren
-        if (anzahlManager <= setOfFactors.size())
+        // der reihe nach wird jeder faktor verteilt, wenn keine Faktoren mehr da sind beginnt er von vorn
+        for (int i = 1; i <= anzahlManager; i++)
         {
-            // jeder manager bekommt einen andern Faktor
-            for (int i = 0; i < anzahlManager; i++)
+            //TODO: *iter an Manager[i] senden
+//                cout << "i: " << i << endl;
+//                cout << "p = PollardRho(*iter); " << *iter << endl;
+            num = worker(*iter);
+
+            if(i % setOfFactors.size() == 0)
             {
-                //TODO: *iter an Manager[i] senden
-                num = worker(*iter);
+                iter= setOfFactors.begin();
+            }
+            else
+            {
                 ++iter;
             }
         }
-        // mehr Manager als Faktoren
-        else
-        {
-            // der reihe nach wird jeder faktor verteilt, wenn keine Faktoren mehr da sind beginnt er von vorn
-            for (int i = 1; i <= anzahlManager; i++)
-            {
-                //TODO: *iter an Manager[i] senden
-//                cout << "i: " << i << endl;
-//                cout << "p = PollardRho(*iter); " << *iter << endl;
-                num = worker(*iter);
-
-                if(i % setOfFactors.size() == 0)
-                {
-                    iter= setOfFactors.begin();
-                }
-                else
-                {
-                    ++iter;
-                }
-            }
-        }
-
-
-
-
-
 
         //todo: Koordinator
         // num.N aus den Faktoren Löschen
@@ -418,8 +392,6 @@ int main()
         setOfFactors.erase(num.N);
     }
 
-
-
     //  Stop timers
     double wallEndTime = get_wall_time();
     double cpuEndTime  = get_cpu_time();
@@ -433,244 +405,6 @@ int main()
     cout << "N: " << N << endl;
     cout << "The Primfactors for N are: ";
     printSet(setOfPrimFactors);
-
     return 0;
 }
-
-
-
-///* a++ program to find a prime factor of composite using
-//   Pollard's Rho algorithm */
-//#include<bits/stdc++.h>
-//#include <boost/multiprecision/cpp_int.hpp>
-//
-//using namespace std;
-//using namespace boost::multiprecision;
-//
-//int512_t PollardRho(int512_t N);
-//inline bool IsPrime( int512_t number );
-//int512_t power(int512_t a, int512_t n, int512_t mod);
-//int512_t modular_pow(int512_t base, int512_t exponent, int512_t modulus);
-//void printSet(set<int512_t> setToPrint);
-//
-//
-//
-///* method to return prime divisor for N */
-//int512_t PollardRho(int512_t N)
-//{
-//    /* initialize random seed */
-//    srand (time(NULL));
-//
-//    if(IsPrime(N))
-//    {
-//        return N;
-//    }
-//
-//    /* no prime divisor for 1 */
-//    if (N==1) return N;
-//
-//    /* even number means one of the divisors is 2 */
-//    if (N % 2 == 0) return 2;
-//
-//    /* we will pick from the range [1, N) */
-//    int512_t x = (rand()% N + 1);
-//
-//    int512_t y = x;
-//
-//    /* the constant in f(x).
-//     * Algorithm can be re-run with a different a
-//     * if it throws failure for a composite. */
-//    int512_t a = (rand()%(N-1))+1;
-//
-//    /* Initialize Difference */
-//    int512_t d = 0;
-//
-//    /* Initialize candidate divisor (or result) */
-//    int512_t p = 1;
-//
-//    /* until the prime factor isn't obtained.
-//       If N is prime, return N */
-//
-//    do
-//    {
-//        /* Tortoise Move: x= (x² + a) mod N */
-//        x = (x * x + a) % N;
-//
-//        /* Hare Move: y(i+1) = f(f(y(i))) */
-//        y = (y * y + a) % N;
-//        y = (y * y + a) % N;
-//
-//        /* Difference between (y - x) % N*/
-//        d = (y - x) % N;
-//
-//        /* check gcd of |x-y| and N */
-//        p = __gcd(d, N);
-//        /* make the divisor positive */
-//        if (p < 0)
-//        {
-//            p *= -1;
-//        }
-//
-//
-//    }
-//    while (p == 1 || p == -1 );
-//
-//    /* retry if the algorithm fails to find prime factor
-//     * with chosen x and a */
-//    if (p!=N)
-//    {
-//        return p;
-//
-//    }
-//}
-//
-//
-//// Check Prime
-/////////////////////////
-//int512_t power(int512_t a, int512_t n, int512_t mod)
-//{
-//    int512_t power = a,result=1;
-//
-//    while(n)
-//    {
-//        if(n&1)
-//            result=(result*power)%mod;
-//        power=(power*power)%mod;
-//        n>>=1;
-//    }
-//    return result;
-//}
-//
-//bool witness(int512_t a, int512_t n)
-//{
-//    int512_t t,u,i;
-//    int512_t prev,curr;
-//
-//    u=n/2;
-//    t=1;
-//    while(!(u&1))
-//    {
-//        u/=2;
-//        ++t;
-//    }
-//
-//    prev=power(a,u,n);
-//    for(i=1; i<=t; ++i)
-//    {
-//        curr=(prev*prev)%n;
-//        if((curr==1)&&(prev!=1)&&(prev!=n-1))
-//            return true;
-//        prev=curr;
-//    }
-//    if(curr!=1)
-//        return true;
-//    return false;
-//}
-//
-//inline bool IsPrime( int512_t number )
-//{
-//    if ( ( (!(number & 1)) && number != 2 ) || (number < 2) || (number % 3 == 0 && number != 3) )
-//        return (false);
-//
-//    if(number<1373653)
-//    {
-//        for( int k = 1; 36*k*k-12*k < number; ++k)
-//            if ( (number % (6*k+1) == 0) || (number % (6*k-1) == 0) )
-//                return (false);
-//
-//        return true;
-//    }
-//
-//    if(number < 9080191)
-//    {
-//        if(witness(31,number)) return false;
-//        if(witness(73,number)) return false;
-//        return true;
-//    }
-//
-//
-//    if(witness(2,number)) return false;
-//    if(witness(7,number)) return false;
-//    if(witness(61,number)) return false;
-//    return true;
-//
-//    /*WARNING: Algorithm deterministic only for numbers < 4,759,123,141 (unsigned int's max is 4294967296)
-//      if n < 1,373,653, it is enough to test a = 2 and 3.
-//      if n < 9,080,191, it is enough to test a = 31 and 73.
-//      if n < 4,759,123,141, it is enough to test a = 2, 7, and 61.
-//      if n < 2,152,302,898,747, it is enough to test a = 2, 3, 5, 7, and 11.
-//      if n < 3,474,749,660,383, it is enough to test a = 2, 3, 5, 7, 11, and 13.
-//      if n < 341,550,071,728,321, it is enough to test a = 2, 3, 5, 7, 11, 13, and 17.*/
-//}
-//
-///////////////////////////
-//
-//
-//void printSet(set<int512_t> setToPrint)
-//{
-//    set<int512_t>::iterator iter;
-//    for(iter=setToPrint.begin(); iter!=setToPrint.end(); ++iter)
-//    {
-//        cout << *iter << " " ;
-//    }
-//    cout << endl;
-//
-//}
-//
-///* driver function */
-//int main()
-//{
-//
-//    set<int512_t> setOfPrimFactors;
-//    set<int512_t> setOfFactors;
-//
-//    //int512_t N = 210;
-//    int512_t N = 8806715679;
-//    int512_t p = 1 ;
-//
-//    setOfFactors.insert(N);
-//    while(N != 1 || !setOfFactors.empty())
-//    {
-//        p = PollardRho(N);
-//
-//        if(IsPrime(p))
-//        {
-//
-//        //TODO: send p to Koordinator
-//            setOfPrimFactors.insert(p);
-//         //TODO: send N to Koordinator? oder weiß der
-//            setOfFactors.erase(N);
-//
-//            //Check what we is to do
-//            if(!setOfFactors.empty())
-//            {
-//                set<int512_t>::iterator oneFactor = setOfFactors.begin();
-//                //TODO: send N to worker
-//                N = *oneFactor;
-//
-//            }
-//        }
-//        else if (N % p == 0)
-//        {
-//            setOfFactors.insert(p);
-//        }
-//        while (N % p == 0)
-//        {
-//            N /= p;
-//        }
-//
-//        if (N > 1 )
-//        {
-//            setOfFactors.insert(N);
-//        }
-//            cout << "N: " << N << endl;
-//
-//    }
-//
-//    // Print setOfPrimFactors
-//    cout << "The Primfactors are: ";
-//    printSet(setOfPrimFactors);
-//
-//    return 0;
-//}
 
