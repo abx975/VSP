0259d385ec74743c519b8746cd514a8cfeef5279
Lennart Hartmann <lennart.hartmann@haw-hamburg.de>
2017-11-09 07:24:06 +0100

    added project folder

---
+++ VSP2/vslab-caf/cmake-build-debug/CMakeFiles/vslab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vslab.dir/src/main.cpp.o"
+  "vslab.pdb"
+  "vslab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vslab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
