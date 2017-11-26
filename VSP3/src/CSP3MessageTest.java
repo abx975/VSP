38f180607993f009cecfa5548de6c7e2115bf148
Nils Eggebrecht <nils.eggebrecht@haw-hamburg.de>
2017-11-26 15:52:05 +0100

    add JUnit tests for Messages and fix getSendTime in Messages

--- VSP3/src/CSP3MessageTest.java
+++ VSP3/src/CSP3MessageTest.java
@@ -1,16 +1,125 @@
+import static org.junit.Assert.*;
+import java.util.Arrays;
 import org.junit.Test;
 
-import junit.framework.TestCase;
+/**
+ * JUnit test class for  CSP3MessageTest
+ * 
+ * @author Nils Eggebrecht
+ * @version 26.11.2017
+ */
 
+public class CSP3MessageTest {
 
-public class CSP3MessageTest extends TestCase {
-	
-	
-	String testStr = "AHalloLeutchen";
-	
 	@Test
-	void testGetMessage(){
-		assert(true);
+	public void testGetStationClassByAorB() {
+
+		char[] ch = { '0' };
+
+		// StationClass = 'A'
+		byte[] retval = VS3Messages.getMessage('A', "ABC", (byte) 1,
+				(long) System.currentTimeMillis());
+		ch[0] = VS3Messages.getStationClass(retval);
+		assertEquals("ch[0] =! 'A'", ch[0], 'A');
+
+		// StationClass = 'B'
+		retval = VS3Messages.getMessage('B', "ABC", (byte) 1,
+				(long) System.currentTimeMillis());
+		ch[0] = VS3Messages.getStationClass(retval);
+		assertEquals("ch[0] =! 'B'", ch[0], 'B');
 	}
-	// usw.
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetStationClassByFoultValue() {
+		byte[] retval = VS3Messages.getMessage('C', "ABC", (byte) 1,
+				(long) System.currentTimeMillis());
+		VS3Messages.getStationClass(retval);
+	}
+
+	@Test
+	public void testGetPayload() {
+		byte[] retval = VS3Messages.getMessage('A', "1234", (byte) 1,
+				System.currentTimeMillis());
+		byte[] ch = VS3Messages.getPayload(retval);
+
+		byte[] a;
+		a = new byte[23];
+		a[0] = '1';
+		a[1] = '2';
+		a[2] = '3';
+		a[3] = '4';
+		for (int i = 0; i < ch.length; i++) {
+			assertEquals("der Paylod ist nicht 1234", ch[i], a[i]);
+		}
+	}
+
+	@Test
+	public void testGetPayloadtobig() {
+		byte[] retval = VS3Messages.getMessage('A',
+				"1234567890123456789012345678", (byte) 1,
+				System.currentTimeMillis());
+		byte[] ch = VS3Messages.getPayload(retval);
+
+		byte[] a;
+		a = new byte[28];
+		a[0] = '1';
+		a[1] = '2';
+		a[2] = '3';
+		a[3] = '4';
+		a[4] = '5';
+		a[5] = '6';
+		a[6] = '7';
+		a[7] = '8';
+		a[8] = '9';
+		a[9] = '0';
+		a[10] = '1';
+		a[11] = '2';
+		a[12] = '3';
+		a[13] = '4';
+		a[14] = '5';
+		a[15] = '6';
+		a[16] = '7';
+		a[17] = '8';
+		a[18] = '9';
+		a[19] = '0';
+		a[20] = '1';
+		a[21] = '2';
+		a[22] = '3';
+		a[23] = '4';
+
+		for (int i = 0; i < ch.length; i++) {
+			assertEquals("die ersten 23byte sind nicht gleich", ch[i], a[i]);
+		}
+	}
+
+	@Test
+	public void testGetString() {
+		String s = "1234                   ";
+		byte[] retval = VS3Messages.getMessage('A', s, (byte) 1,
+				System.currentTimeMillis());
+		String str = VS3Messages.getString(retval);
+		assertEquals("Die Nachricht ist zu klein", s, str);
+	}
+
+	@Test
+	public void testgetNextSlotScheduled() {
+		String s = "1234                   ";
+		byte[] retval = VS3Messages.getMessage('A', s, (byte) 2,
+				System.currentTimeMillis());
+		int i = VS3Messages.getNextSlotScheduled(retval);
+		assertEquals("Die Nachricht ist zu klein", i, 2);
+	}
+
+	@Test
+	public void testgetSendTime() {
+		long time = System.currentTimeMillis();
+		byte[] retval = VS3Messages.getMessage('A', "1234", (byte) 1, time);
+		long timeread = 0;
+		System.out.println("print time: ");
+		timeread = VS3Messages.getSendTime(retval);
+		System.out.println(timeread);
+		System.out.println(time);
+		assertEquals("Die Nachricht ist zu klein", timeread, time);
+	}
+	
 }
--- VSP3/src/VS3Messages.java
+++ VSP3/src/VS3Messages.java
@@ -5,96 +5,128 @@ import java.lang.IllegalArgumentException;
 import java.util.Arrays;
 
 /**
- * Helper class for managing messages adhering to protocol specified in VS-Lab3 
- * @author lennarthartmann
- * @version 25.11.2017
+ * Helper class for managing messages adhering to protocol specified in VS-Lab3
+ * 
+ * @author Lennart Hartmann, Nils Eggebrecht
+ * @version 26.11.2017
  */
 public class VS3Messages {
-	
+
 	public static final int MESSAGE_SIZE = 34;
 	public static final int STATION_TYPE_IDX = 0;
 	public static final int PAYLOAD_START = 1;
 	public static final int PAYLOAD_END = 24;
 	public static final int NEXT_SLOT_IDX = 25;
 	public static final int SEND_TIME_START = 26;
+	public static final int SEND_TIME_END = 33;
+
 	public static final int ZERO_IDX = 0;
-	
+
 	/**
 	 * Retrieves the sending station's type
-	 * @param input the stream to be checked
+	 * 
+	 * @param input
+	 *            the stream to be checked
 	 * @return the sender's class
 	 * @throws IllegalArgumentException
 	 */
-	public static char getStationClass(byte[] input) throws IllegalArgumentException{
-		if(input.length!=MESSAGE_SIZE){
-			throw new IllegalArgumentException("A valid message needs to have 34 bytes");
+	public static char getStationClass(byte[] input)
+			throws IllegalArgumentException {
+		if (input.length != MESSAGE_SIZE) {
+			throw new IllegalArgumentException(
+					"A valid message needs to have 34 bytes");
 		}
-		if(input[ZERO_IDX]=='A'){
+		if (input[ZERO_IDX] == 'A') {
 			return 'A';
-		}else if(input[ZERO_IDX]=='B'){
+		} else if (input[ZERO_IDX] == 'B') {
 			return 'B';
-		}else{
-			throw new IllegalArgumentException("Station type needs to be 'A' or 'B'");
+		} else {
+			throw new IllegalArgumentException(
+					"Station type needs to be 'A' or 'B'");
 		}
 	}
-	
+
 	/**
-	 * Retrieves the message's payload
-	 * @param input	the stream to be checked
-	 * @return	the payload
+	 * Retrieves the message public static final int SEND_TIME_END = 33; 's
+	 * payload
+	 * 
+	 * @param input
+	 *            the stream to be checked
+	 * @return the payload
 	 */
-	public static byte[] getPayload(byte[] input){
-		if(input.length!=MESSAGE_SIZE){
-			throw new IllegalArgumentException("A valid message needs to have 34 bytes");
+	public static byte[] getPayload(byte[] input) {
+		if (input.length != MESSAGE_SIZE) {
+			throw new IllegalArgumentException(
+					"A valid message needs to have 34 bytes");
 		}
 		return Arrays.copyOfRange(input, PAYLOAD_START, PAYLOAD_END);
 	}
-	
+
 	/**
 	 * Retrieves the message's payload as String
-	 * @param input	the stream to be checked
-	 * @return	the payload
+	 * 
+	 * @param input
+	 *            the stream to be checked
+	 * @return the payload
 	 */
-	public static String getString(byte[] input){
+	public static String getString(byte[] input) {
 		return new String(getPayload(input));
 	}
-	
+
 	/**
 	 * Retrieves the slot the message's sender intends to use next
+	 * 
 	 * @return the slot intended
 	 */
-	public static int getNextSlotScheduled(byte[] input){
-		if(input.length!=MESSAGE_SIZE){
-			throw new IllegalArgumentException("A valid message needs to have 34 bytes");
+	public static int getNextSlotScheduled(byte[] input) {
+		if (input.length != MESSAGE_SIZE) {
+			throw new IllegalArgumentException(
+					"A valid message needs to have 34 bytes");
 		}
-		return (int)input[NEXT_SLOT_IDX];
+		return (int) input[NEXT_SLOT_IDX];
 	}
-	
+
 	/**
 	 * Retrieves the time the message was sent
+	 * 
 	 * @return the slot intended
 	 */
-	public static long getSendTime(byte[] input){
-		if(input.length!=MESSAGE_SIZE){
-			throw new IllegalArgumentException("A valid message needs to have 34 bytes");
+	public static long getSendTime(byte[] input) {
+		if (input.length != MESSAGE_SIZE) {
+			throw new IllegalArgumentException(
+					"A valid message needs to have 34 bytes");
 		}
-		byte[] time = Arrays.copyOfRange(input, SEND_TIME_START,MESSAGE_SIZE);
-		return Long.parseLong(new String(time));
+		byte[] time = Arrays.copyOfRange(input, SEND_TIME_START, SEND_TIME_END);
+
+		long retval = 0;
+		for (int i = 0; i < input.length; i++) {
+			retval = (retval << 8) + (input[i] & 0xff);
+		}
+
+		return retval;
+
+		// return Long.parseLong(new String(time));
 	}
-	
+
 	/**
-	 * Creates a new message according to protocol 
-	 * @param type	the sending station's type
-	 * @param message	the payload as a String
-	 * @param nextSlot	the next slot intended to be used by sender
-	 * @param sendTime	the time the message was sent
-	 * @return	the message created
+	 * Creates a new message according to protocol
+	 * 
+	 * @param type
+	 *            the sending station's type
+	 * @param message
+	 *            the payload as a String
+	 * @param nextSlot
+	 *            the next slot intended to be used by sender
+	 * @param sendTime
+	 *            the time the message was sent
+	 * @return the message created
 	 */
-	public static byte[] getMessage(char type, String payload, byte nextSlot, long sendTime){
+	public static byte[] getMessage(char type, String payload, byte nextSlot,
+			long sendTime) {
 		byte[] sendTimeBytes;
 		byte[] msg = new byte[MESSAGE_SIZE];
-		
-		//convert long to byte[]
+
+		// convert long to byte[]
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(baos);
 		try {
@@ -113,41 +145,49 @@ public class VS3Messages {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//write type
-		msg[STATION_TYPE_IDX]=(byte)type;
-		
-		//write payload
-		System.arraycopy(payload.getBytes(), ZERO_IDX, msg, PAYLOAD_START, payload.length());
-				
-		//write nextSlot
-		msg[NEXT_SLOT_IDX]=(byte)nextSlot;
-		
-		//write sendtime
-		System.arraycopy(sendTimeBytes, ZERO_IDX, msg, SEND_TIME_START, sendTimeBytes.length);
 
-//		//reverse byte order: required?
-//		byte[] reverseMsg = new byte[MESSAGE_SIZE];
-//		for(int i=0; i< MESSAGE_SIZE; i++){
-//			reverseMsg[i] = msg[(MESSAGE_SIZE-1)-i];
-//		}
-//		return reverseMsg;
+		// write type
+		msg[STATION_TYPE_IDX] = (byte) type;
+
+		// write payload
+		System.arraycopy(payload.getBytes(), ZERO_IDX, msg, PAYLOAD_START,
+				payload.length());
+
+		// write nextSlot
+		msg[NEXT_SLOT_IDX] = (byte) nextSlot;
+
+		// write sendtime
+		System.arraycopy(sendTimeBytes, ZERO_IDX, msg, SEND_TIME_START,
+				sendTimeBytes.length);
+
+		// //reverse byte order: required?
+		// byte[] reverseMsg = new byte[MESSAGE_SIZE];
+		// for(int i=0; i< MESSAGE_SIZE; i++){
+		// reverseMsg[i] = msg[(MESSAGE_SIZE-1)-i];
+		// }
+		// return reverseMsg;
 		return msg;
 	}
-	
+
 	/**
-	 * Creates a new message according to protocol 
-	 * @param type	the sending station's type
-	 * @param message	the payload
-	 * @param nextSlot	the next slot intended to be used by sender
-	 * @param sendTime	the time the message was sent
-	 * @return	the message created
+	 * Creates a new message according to protocol
+	 * 
+	 * @param type
+	 *            the sending station's type
+	 * @param message
+	 *            the payload
+	 * @param nextSlot
+	 *            the next slot intended to be used by sender
+	 * @param sendTime
+	 *            the time the message was sent
+	 * @return the message created
 	 */
-	public static byte[] getMessage(char type, byte[] payload, byte nextSlot, long sendTime){
+	public static byte[] getMessage(char type, byte[] payload, byte nextSlot,
+			long sendTime) {
 		byte[] sendTimeBytes;
 		byte[] msg = new byte[MESSAGE_SIZE];
-		
-		//convert long to byte[]
+
+		// convert long to byte[]
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(baos);
 		try {
@@ -166,18 +206,19 @@ public class VS3Messages {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//write type
-		msg[STATION_TYPE_IDX]=(byte)type;
-		
-		//write payload
+
+		// write type
+		msg[STATION_TYPE_IDX] = (byte) type;
+
+		// write payload
 		System.arraycopy(payload, ZERO_IDX, msg, PAYLOAD_START, payload.length);
-				
-		//write nextSlot
-		msg[NEXT_SLOT_IDX]=(byte)nextSlot;
-		
-		//write sendtime
-		System.arraycopy(sendTimeBytes, ZERO_IDX, msg, SEND_TIME_START, sendTimeBytes.length);
+
+		// write nextSlot
+		msg[NEXT_SLOT_IDX] = (byte) nextSlot;
+
+		// write sendtime
+		System.arraycopy(sendTimeBytes, ZERO_IDX, msg, SEND_TIME_START,
+				sendTimeBytes.length);
 		return msg;
 	}
 }
